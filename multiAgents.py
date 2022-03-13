c8a80281e406dcab3536977404c85960adf9c2e5
Jason Li <jasonli@Jasons-MacBook-Pro.local>
2022-03-12 21:45:43 -0800

    edited fooddist weight

--- multiAgents.py
+++ multiAgents.py
@@ -184,7 +184,7 @@ class MultiPacmanAgent(MultiAgentSearchAgent):
             if (ghostDist < 3):
                 return -float(inf)
 
-        return 1000000.0/(currentGameState.getNumFood() + 1) + ghostDist + 1000.0/(fooddist + 1) + 10000.0/(len(currentGameState.getCapsules()) + 1)
+        return 1000000.0/(currentGameState.getNumFood() + 1) + ghostDist + 100.0/(fooddist + 1) + 10000.0/(len(currentGameState.getCapsules()) + 1)
         
 class RandomAgent(MultiAgentSearchAgent):
     def getAction(self, gameState):
